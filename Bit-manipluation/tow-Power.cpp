4bd958d327fb6a295bdf709f2c6ae553e6a6319f
Ankush <Ankushkushwah307@gmail.com>
2026-04-05 23:38:55 +0530

    solved power of four problem

--- Bit-manipluation/tow-Power.cpp
+++ Bit-manipluation/tow-Power.cpp
@@ -6,6 +6,16 @@ bool isPowerOfTwo(int n) {
     return (n & (n - 1)) == 0;
 }
 
+bool isPowerOfFour(int n) {
+    if (n <= 0) return false;
+
+    // condition 1: power of 2
+    if ((n & (n - 1)) != 0) return false;
+
+    // condition 2: bit at even position
+    return (n & 0x55555555);
+}
+
 int main() {
     int n;
     cout << "Enter number: ";
@@ -15,6 +25,11 @@ int main() {
         cout << "Yes, Power of 2\n";
     else
         cout << "No, Not Power of 2\n";
+
+        if (isPowerOfFour(n))
+        cout << "Yes, Power of 4\n";
+    else
+        cout << "No, Not Power of 4\n";
 
     return 0;
 }
