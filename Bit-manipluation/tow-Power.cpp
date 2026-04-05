3a6578e898a47b47e6d75e2022c79f2eb02ee259
Ankush <Ankushkushwah307@gmail.com>
2026-04-05 23:36:44 +0530

    Solved power of two problem

---
+++ Bit-manipluation/tow-Power.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+bool isPowerOfTwo(int n) {
+    if (n <= 0) return false;
+    return (n & (n - 1)) == 0;
+}
+
+int main() {
+    int n;
+    cout << "Enter number: ";
+    cin >> n;
+
+    if (isPowerOfTwo(n))
+        cout << "Yes, Power of 2\n";
+    else
+        cout << "No, Not Power of 2\n";
+
+    return 0;
+}
