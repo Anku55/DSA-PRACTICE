f45687e1cff8b917d354bad18c0213dd719bf9a2
Ankush <Ankushkushwah307@gmail.com>
2026-04-06 23:51:18 +0530

    solved compliment problem on leetcode(476)

---
+++ Bit-manipluation/Compliment.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int findComplement(int num) {
+        int mask = 0;
+        int temp = num;
+
+        // Step 1: Create mask with all bits = 1
+        while (temp > 0) {
+            mask = (mask << 1) | 1;
+            temp >>= 1;
+        }
+
+        // Step 2: XOR with mask to flip bits
+        return num ^ mask;
+    }
+};
+
+int main() {
+    Solution obj;
+    
+    int num = 5;
+    cout << "Complement: " << obj.findComplement(num) << endl;
+
+    return 0;
+}
