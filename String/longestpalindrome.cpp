64f7c14c1102afcc517b729203354959be3b2264
Ankush <Ankushkushwah307@gmail.com>
2026-04-07 22:51:38 +0530

    longest palindromic substring

---
+++ String/longestpalindrome.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int n = s.length();
+        int start = 0, maxLen = 1;
+
+        for (int i = 0; i < n; i++) {
+            // Odd length palindrome
+            expand(s, i, i, start, maxLen);
+
+            // Even length palindrome
+            expand(s, i, i + 1, start, maxLen);
+        }
+
+        return s.substr(start, maxLen);
+    }
+
+private:
+    void expand(string &s, int left, int right, int &start, int &maxLen) {
+        while (left >= 0 && right < s.length() && s[left] == s[right]) {
+            int len = right - left + 1;
+
+            if (len > maxLen) {
+                start = left;
+                maxLen = len;
+            }
+
+            left--;
+            right++;
+        }
+    }
+};
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    Solution obj;
+
+    string s;
+    cout << "Enter string: ";
+    cin >> s;
+
+    string result = obj.longestPalindrome(s);
+
+    cout << "Longest Palindromic Substring: " << result << "\n";
+
+    return 0;
+}
