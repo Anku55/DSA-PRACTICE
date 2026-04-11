f4ba56ac5ec55d7d91d22610d5911defd8c59468
Ankush <Ankushkushwah307@gmail.com>
2026-04-11 23:46:05 +0530

    solved lc 33

---
+++ Array/lc-33.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int low = 0, high = nums.size() - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target)
+                return mid;
+
+            // Left half sorted
+            if (nums[low] <= nums[mid]) {
+                if (nums[low] <= target && target < nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            }
+            // Right half sorted
+            else {
+                if (nums[mid] < target && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+};
+
+int main() {
+    Solution obj;
+    vector<int> nums = {4,5,6,7,0,1,2};
+    int target = 0;
+
+    cout << obj.search(nums, target) << endl;
+    return 0;
+}
