260d46231dc33e42ab95c78c888f249e13b65896
Ankush <Ankushkushwah307@gmail.com>
2026-04-12 19:00:56 +0530

    solved leetcode 2433

--- Bit-manipluation/bit-Tricks.cpp
+++ Bit-manipluation/bit-Tricks.cpp
@@ -1,5 +1,6 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
+
 
 int hammingWeight(unsigned int n) {
     int cnt = 0;
@@ -11,12 +12,27 @@ int hammingWeight(unsigned int n) {
 }
 
 int main() {
-    unsigned int n;
+    // -------- Prefix XOR Problem --------
+    vector<int> pref = {5, 2, 0, 3, 1};
+    int n = pref.size();
 
+    vector<int> arr(n);
+    arr[0] = pref[0];
+
+    for (int i = 1; i < n; i++) {
+        arr[i] = pref[i] ^ pref[i - 1];
+    }
+
+    cout << "Reconstructed array: ";
+    for (int x : arr) cout << x << " ";
+    cout << "\n";
+
+    // -------- Hamming Weight Problem --------
+    unsigned int num;
     cout << "Enter number: ";
-    cin >> n;
+    cin >> num;
 
-    cout << "Number of set bits: " << hammingWeight(n) << endl;
+    cout << "Number of set bits: " << hammingWeight(num) << "\n";
 
     return 0;
 }
