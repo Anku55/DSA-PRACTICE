c27ed980199a593cd7c72696f5475f04c7223111
Ankush <Ankushkushwah307@gmail.com>
2026-04-13 23:56:18 +0530

    leetcode 29

---
+++ Bit-manipluation/lc-29.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <climits>   // 🔥 REQUIRED for INT_MIN and INT_MAX
+using namespace std;
+
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+        if (dividend == INT_MIN && divisor == -1)
+            return INT_MAX;
+
+        long long dvd = abs((long long)dividend);
+        long long dvs = abs((long long)divisor);
+
+        int result = 0;
+
+        while (dvd >= dvs) {
+            long long temp = dvs, multiple = 1;
+
+            while (dvd >= (temp << 1)) {
+                temp <<= 1;
+                multiple <<= 1;
+            }
+
+            dvd -= temp;
+            result += multiple;
+        }
+
+        if ((dividend < 0) ^ (divisor < 0))
+            result = -result;
+
+        return result;
+    }
+};
+
+int main() {
+    Solution obj;
+    cout << obj.divide(10, 3);
+}
