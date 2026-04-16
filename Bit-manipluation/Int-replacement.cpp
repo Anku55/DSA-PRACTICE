d6afe1ddb0201d837709b0748f54e09e0b605d8e
Ankush <Ankushkushwah307@gmail.com>
2026-04-16 22:53:36 +0530

    solved integer replacement problem on lc

---
+++ Bit-manipluation/Int-replacement.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+int integerRepcement(int n){
+    long long num=n;
+    int count =0;
+    while(num != 1){
+        if((num & 1)==0){
+
+            num=num/2;
+
+
+        }else {
+            if(num==3 || ((num>>1)&1)==0){
+                num=num-1;
+            } else{
+                num=num+1;
+            }
+
+        }
+        count ++;
+    }
+
+    return count;
+
+}
+int main(){
+    int n; 
+    cin>>n;
+    cout<<" Min operation require to number become one "<<integerRepcement(n);
+
+
+}
