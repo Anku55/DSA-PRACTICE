dc65c1f99323cb13c366248a8e0a26381ecc964b
Ankush <Ankushkushwah307@gmail.com>
2026-04-18 23:14:13 +0530

    implemented stack

---
+++ Stack/implimentation.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+
+#define MAX 100
+
+class Stack {
+    int arr[MAX];
+    int top;
+
+public:
+    Stack() {
+        top = -1;
+    }
+
+    // Push operation
+    void push(int x) {
+        if (top == MAX - 1) {
+            cout << "Stack Overflow\n";
+            return;
+        }
+        arr[++top] = x;
+        cout << x << " pushed into stack\n";
+    }
+
+    // Pop operation
+    void pop() {
+        if (top == -1) {
+            cout << "Stack Underflow\n";
+            return;
+        }
+        cout << arr[top--] << " popped from stack\n";
+    }
+
+    // Peek operation
+    int peek() {
+        if (top == -1) {
+            cout << "Stack is empty\n";
+            return -1;
+        }
+        return arr[top];
+    }
+
+    bool isEmpty() {
+        return (top == -1);
+    }
+};
+
+int main() {
+    Stack s;
+    s.push(10);
+    s.push(20);
+    s.push(30);
+
+    cout << "Top element: " << s.peek() << endl;
+
+    s.pop();
+    s.pop();
+
+    return 0;
+}
