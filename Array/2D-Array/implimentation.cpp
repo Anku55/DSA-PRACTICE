047d8388ebbbda06d31f639b899f7a34db4ed6e6
Ankush <Ankushkushwah307@gmail.com>
2026-04-28 20:29:23 +0530

    wrote a function to search in the array

--- Array/2D-Array/implimentation.cpp
+++ Array/2D-Array/implimentation.cpp
@@ -1,11 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+bool isPresent(int arr[][4],int target ,int row , int col){
+    for(int col=0;col<4;col++){
+        for(int row=0;row<3;row++){
+            if(arr[row][col]==target)return true;
+        }
+    }
+    return false;
+}
+
 int main(){
     // initializing a 2-D Array
     int arr[3][4];
     // taking input 
     //- row wise
+
+    cout<<"Enter the element of array \n";
+
     for(int i=0;i<3;i++){
         for(int j=0;j<4;j++){
             cin>>arr[i][j];
@@ -17,19 +29,28 @@ int main(){
         }
         cout<<"\n";
     }
+
+    cout<<"Enter the element you want to search \n";
+    int target;
+    cin>>target;
+    if(isPresent(arr , target, 3,4)){
+        cout<<"Element is present";
+    }else{
+        cout<<"element is not present ";
+    }
     // taking column wise input
 
-    for(int i=0;i<4;i++){
-        for(int j=0;j<3;j++){
-            cin>>arr[j][i];
-        }
-    }
-    
-        for(int i=0;i<3;i++){
-        for(int j=0;j<4;j++){
-            cout<< arr[i][j]<<" ";
-        }
-        cout<<"\n";
-    }
+    // for(int i=0;i<4;i++){
+    //     for(int j=0;j<3;j++){
+    //         cin>>arr[j][i];
+    //     }
+    // }
+
+    //     for(int i=0;i<3;i++){
+    //     for(int j=0;j<4;j++){
+    //         cout<< arr[i][j]<<" ";
+    //     }
+    //     cout<<"\n";
+    // }
 
 }
