e07328cbf62f8fb3e69bb63eb6c385414aa0061c
Ankush <Ankushkushwah307@gmail.com>
2026-06-11 18:14:10 +0530

    solved frog jump problem on gfg

---
+++ DynamicProgramming/FrogJump.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+class Solution {
+  public:
+    int fun(int n,vector<int>&height,vector<int>&dp){
+        if(n==0)return 0;
+        if(n<0)return 0;
+        if(dp[n]!=-1)return dp[n];
+        int one=abs(height[n]-height[n-1])+fun(n-1,height,dp);
+        int two=INT_MAX;
+        
+        if(n>1)two=abs(height[n]-height[n-2])+fun(n-2,height,dp);
+        
+        return dp[n]=min(one,two);
+    }
+    int minCost(vector<int>& height) {
+        int n=height.size();
+        vector<int>dp(n+1,-1);
+        
+        return fun(n-1,height,dp);
+        // Code here
+        
+    }
+};
