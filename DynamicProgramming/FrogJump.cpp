f84552ee2f3530d3daf843322f8129d7ed38e6a1
Ankush <Ankushkushwah307@gmail.com>
2026-06-11 18:16:15 +0530

    Recursive way of frog jump problem

--- DynamicProgramming/FrogJump.cpp
+++ DynamicProgramming/FrogJump.cpp
@@ -1,3 +1,25 @@
+############## Recursive way########
+#include <bits/stdc++.h>
+class Solution {
+  public:
+    int fun(int n,vector<int>&height){
+        if(n==0)return 0;
+        if(n<0)return 0;
+        int one=abs(height[n]-height[n-1])+fun(n-1,height);
+        int two=INT_MAX;
+        
+        if(n>1)two=abs(height[n]-height[n-2])+fun(n-2,height);
+        
+        return min(one,two);
+    }
+    int minCost(vector<int>& height) {
+        int n=height.size();
+        return fun(n-1,height);
+        // Code here
+        
+    }
+};
+############### memoaization way##########
 #include <bits/stdc++.h>
 class Solution {
   public:
