8d5396c406d3d211dd6c2fa3e1b52d26d44787e8
Ankush <Ankushkushwah307@gmail.com>
2026-06-11 18:31:53 +0530

    optimize the space

--- DynamicProgramming/FrogJump.cpp
+++ DynamicProgramming/FrogJump.cpp
@@ -43,3 +43,63 @@ class Solution {
         
     }
 };
+
+############## Tabulation ###########
+
+#include <bits/stdc++.h>
+class Solution {
+	public:
+// 	int fun(int n, vector<int>&height, vector<int>&dp) {
+
+	int minCost(vector<int>& height) {
+		int n = height.size();
+		vector<int>dp(n , -1);
+		
+		dp[0] = 0;
+		for (int i = 1; i<n; i++) {
+			
+			int one = abs(height[i]-height[i - 1]) + dp[i-1];
+			int two = INT_MAX;
+			
+			if (i>1)two = abs(height[i]-height[i - 2]) + dp[i-2];
+			
+			dp[i]=min(one,two);
+			
+		}
+		// Code here
+		
+		return dp[n-1];
+		
+	}
+};
+
+#include <bits/stdc++.h>
+class Solution {
+	public:
+
+	int minCost(vector<int>& height) {
+		int n = height.size();
+		vector<int>dp(n , -1);
+		
+	    int prev2=0;
+	    int prev1=0;
+	    
+		for (int i = 1; i<n; i++) {
+			
+			int one = abs(height[i]-height[i - 1]) + prev1;
+			int two = INT_MAX;
+			
+			if (i>1)two = abs(height[i]-height[i - 2]) + prev2;
+			
+			int cur=min(one,two);
+			
+			prev2=prev1;
+			prev1=cur;
+			
+		}
+		// Code here
+		
+		return prev1;
+		
+	}
+};
